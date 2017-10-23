3371f168fc554246b64370ef90abd734b7a96c30
fabsouto <fabsouto@cl.ibm.com>
2017-10-23 18:20:36 -0300

    Update plate script. Working.

--- plate_generator.py
+++ plate_generator.py
@@ -2,8 +2,10 @@ from PIL import Image, ImageDraw, ImageFont
 
 # CONFIG VARS
 font_filepath = "data/font/cargoplaindemo.ttf"
-font_size = 80
+font_size = 300
 plate_filepath = "data/base_plate/plate_without_background.png"
+letters = "A"
+numbers = "1"
 
 # Get the image
 base = Image.open(plate_filepath).convert('RGBA')
@@ -17,8 +19,17 @@ font = ImageFont.truetype(font_filepath, font_size)
 d = ImageDraw.Draw(txt)
 
 # draw text, half opacity
-d.text((100, 100), "BB", font=font, fill=(0, 0, 0, 255))
+for l1 in letters:
+    for l2 in letters:
+        for l3 in letters:
+            for l4 in letters:
+                for n1 in numbers:
+                    for n2 in numbers:
+                        d.text((100, 15), l1 + l2, font=font, fill=(0, 0, 0, 255))
+                        d.text((500, 15), l3 + l4, font=font, fill=(0, 0, 0, 255))
+                        d.text((950, 15), n1 + n2, font=font, fill=(0, 0, 0, 255))
 
-out = Image.alpha_composite(base, txt)
+                        out = Image.alpha_composite(base, txt)
 
-out.show()
+                        out.show()
+                        break
