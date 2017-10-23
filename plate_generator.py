d2815e0f178d049e722bdf97f9f252889e338473
fabsouto <fabsouto@cl.ibm.com>
2017-10-23 18:47:37 -0300

    Generate some plates with the script.

--- plate_generator.py
+++ plate_generator.py
@@ -1,22 +1,17 @@
 from PIL import Image, ImageDraw, ImageFont
 
 # CONFIG VARS
-font_filepath = "data/font/cargoplaindemo.ttf"
-font_size = 300
-plate_filepath = "data/base_plate/plate_without_background.png"
-letters = "A"
-numbers = "1"
+data_dir = "data"
 
-# Get the image
-base = Image.open(plate_filepath).convert('RGBA')
+font_filepath = "{0}/font/EuroPlate.ttf".format(data_dir)
+font_size = 325
 
-# Make a blank image for the text, initialized to transparent text color
-txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
+plate_filepath = "{0}/base_plate/plate_without_background.png".format(data_dir)
 
-# Get the font
-font = ImageFont.truetype(font_filepath, font_size)
-# Get a drawing context
-d = ImageDraw.Draw(txt)
+result_dir = "{0}/generated_plates".format(data_dir)
+
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "1234567890"
 
 # draw text, half opacity
 for l1 in letters:
@@ -25,11 +20,20 @@ for l1 in letters:
             for l4 in letters:
                 for n1 in numbers:
                     for n2 in numbers:
-                        d.text((100, 15), l1 + l2, font=font, fill=(0, 0, 0, 255))
-                        d.text((500, 15), l3 + l4, font=font, fill=(0, 0, 0, 255))
-                        d.text((950, 15), n1 + n2, font=font, fill=(0, 0, 0, 255))
+                        # Get the image
+                        base = Image.open(plate_filepath).convert('RGBA')
+
+                        # Make a blank image for the text, initialized to transparent text color
+                        txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
+
+                        # Get the font
+                        font = ImageFont.truetype(font_filepath, font_size)
+                        # Get a drawing context
+                        d = ImageDraw.Draw(txt)
+                        d.text((100, 60), l1 + l2, font=font, fill=(0, 0, 0, 255))
+                        d.text((500, 60), l3 + l4, font=font, fill=(0, 0, 0, 255))
+                        d.text((950, 60), n1 + n2, font=font, fill=(0, 0, 0, 255))
 
                         out = Image.alpha_composite(base, txt)
 
-                        out.show()
-                        break
+                        out.save("{0}/{1}".format(result_dir, l1 + l2 + l3 + l4 + n1 + n2 + ".png"), "PNG")
