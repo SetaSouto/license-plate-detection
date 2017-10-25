62d670d29abd0452fac40b6bd70450a2a387cfac
fabsouto <fabsouto@cl.ibm.com>
2017-10-25 14:34:48 -0300

    Save labels for the license plate in YOLO format

--- plate_generator.py
+++ plate_generator.py
@@ -1,5 +1,7 @@
-from PIL import Image, ImageDraw, ImageFont
 from random import choice
+
+from PIL import Image, ImageDraw, ImageFont
+from yolo_categories import CATEGORIES
 
 
 class PlateGenerator():
@@ -23,7 +25,8 @@ class PlateGenerator():
         return choice(self.letters), choice(self.letters), choice(self.letters), choice(self.letters), choice(
             self.numbers), choice(self.numbers)
 
-    def generate_plate(self, l1, l2, l3, l4, n1, n2, show_bounding_box=False, show=False, save=True):
+    def generate_plate(self, l1, l2, l3, l4, n1, n2, show_bounding_box=False, show=False, save=True,
+                       debug_bounding_box_normalized=False):
         """
         Given 4 letters and 2 numbers generate a license plate.
         Saves the image as a PNG in the results dir.
@@ -47,27 +50,89 @@ class PlateGenerator():
             draw.text((x, y), c, font=font, fill=(0, 0, 0, 255))
 
         if show_bounding_box:
-            x_offset = 5
-            y_offset = 10
-            width = 170
-            height = 255
             for x, y, _ in coordinates_and_chars:
-                draw.rectangle([x + x_offset, y + y_offset, x + width, y + height], outline="black")
+                draw.rectangle(self.get_bounding_box(x, y), outline="black")
+
+        if debug_bounding_box_normalized:
+            for x, y, _ in coordinates_and_chars:
+                center_x, center_y, width, height = self.get_bounding_box_normalized(x, y, base.size)
+                x0 = (center_x - width / 2) * base.size[0]
+                x1 = (center_x + width / 2) * base.size[0]
+                y0 = (center_y - height / 2) * base.size[1]
+                y1 = (center_y + height / 2) * base.size[1]
+                draw.rectangle([x0, y0, x1, y1], outline="black")
 
         out = Image.alpha_composite(base, txt)
 
         if show: out.show()
 
-        if save: out.save("{0}/{1}".format(self.result_dir, l1 + l2 + l3 + l4 + n1 + n2 + ".png"), "PNG")
+        if save:
+            out.save("{0}/{1}".format(self.result_dir, l1 + l2 + l3 + l4 + n1 + n2 + ".png"), "PNG")
+            self.save_label(coordinates_and_chars, out.size)
 
-    def generate_random_plates(self, n, show_bounding_box=False, show=False, save=True):
+    def generate_random_plates(self, n, show_bounding_box=False, show=False, save=True,
+                               debug_bounding_box_normalized=False):
         """
         Generates n license plates randomly.
         """
         for _ in range(n):
             l1, l2, l3, l4, n1, n2 = self.get_chars()
-            self.generate_plate(l1, l2, l3, l4, n1, n2, show_bounding_box=show_bounding_box, show=show, save=save)
+            self.generate_plate(l1, l2, l3, l4, n1, n2, show_bounding_box=show_bounding_box, show=show, save=save,
+                                debug_bounding_box_normalized=debug_bounding_box_normalized)
+
+    def save_label(self, coordinates_and_chars, image_size):
+        """
+        Receives the tuples with the x left, y top and char a
+        :param coordinates_and_chars: List of tuples as (int, int, char).
+        :param image_size: Tuple as (width, height)
+        """
+
+        def get_string(x, y, c):
+            """
+            Given a position (x left, y top) and a char returns the string to write as a YOLO format:
+            "category center_x center_y width height"
+            """
+            center_x, center_y, width, height = self.get_bounding_box_normalized(x, y, image_size)
+            category = CATEGORIES[c]
+            return "{0} {1} {2} {3} {4}\n".format(category, center_x, center_y, width, height)
+
+        refactor = self.result_dir + "/" + "".join([tuple[2] for tuple in coordinates_and_chars]) + ".txt"
+        with open(refactor, "w") as f:
+            for x, y, c in coordinates_and_chars:
+                f.write(get_string(x, y, c))
+
+    def get_bounding_box(self, x, y):
+        """
+        Returns the bounding box coordinates for a position given where x is the left side and y is te top side.
+        Returns [x left, y top, x right, y bottom]
+        """
+        x_offset = 5
+        y_offset = 10
+        width = 170
+        height = 255
+
+        return [x + x_offset, y + y_offset, x + width, y + height]
+
+    def get_bounding_box_normalized(self, x, y, image_size):
+        # Get bounding box
+        bounding_box = self.get_bounding_box(x, y)
+        # Get parameters
+        width = bounding_box[2] - bounding_box[0]
+        center_x = bounding_box[0] + (width / 2)
+        height = bounding_box[3] - bounding_box[1]
+        center_y = bounding_box[1] + (height / 2)
+        # Relative to image size
+        width /= image_size[0]
+        height /= image_size[1]
+        center_x /= image_size[0]
+        center_y /= image_size[1]
+
+        return center_x, center_y, width, height
 
 
 # ----- MAIN SCRIPT ----- #
-PlateGenerator().generate_random_plates(1, show_bounding_box=True, show=True, save=False)
+PlateGenerator().generate_random_plates(10,
+                                        show_bounding_box=False,
+                                        show=False,
+                                        save=True,
+                                        debug_bounding_box_normalized=False)
