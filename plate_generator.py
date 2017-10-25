922e0e4b3f2c0a1f44d80d894e71bc34e8ed9da4
fabsouto <fabsouto@cl.ibm.com>
2017-10-25 13:36:54 -0300

    Create class PlateGenerator and add bounding boxes to show.
    Also, add yolo categories

--- plate_generator.py
+++ plate_generator.py
@@ -1,39 +1,73 @@
 from PIL import Image, ImageDraw, ImageFont
+from random import choice
 
-# CONFIG VARS
-data_dir = "data"
 
-font_filepath = "{0}/font/EuroPlate.ttf".format(data_dir)
-font_size = 325
+class PlateGenerator():
+    # CONFIG VARS
+    data_dir = "data"
 
-plate_filepath = "{0}/base_plate/plate_without_background.png".format(data_dir)
+    font_filepath = "{0}/font/EuroPlate.ttf".format(data_dir)
+    font_size = 325
 
-result_dir = "{0}/generated_plates".format(data_dir)
+    plate_filepath = "{0}/base_plate/plate_without_background.png".format(data_dir)
 
-letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "1234567890"
+    result_dir = "{0}/generated_plates".format(data_dir)
 
-# draw text, half opacity
-for l1 in letters:
-    for l2 in letters:
-        for l3 in letters:
-            for l4 in letters:
-                for n1 in numbers:
-                    for n2 in numbers:
-                        # Get the image
-                        base = Image.open(plate_filepath).convert('RGBA')
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "1234567890"
 
-                        # Make a blank image for the text, initialized to transparent text color
-                        txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
+    def get_chars(self):
+        """
+        Returns a tuple with 4 letters and 2 numbers randomly selected.
+        """
+        return choice(self.letters), choice(self.letters), choice(self.letters), choice(self.letters), choice(
+            self.numbers), choice(self.numbers)
 
-                        # Get the font
-                        font = ImageFont.truetype(font_filepath, font_size)
-                        # Get a drawing context
-                        d = ImageDraw.Draw(txt)
-                        d.text((100, 60), l1 + l2, font=font, fill=(0, 0, 0, 255))
-                        d.text((500, 60), l3 + l4, font=font, fill=(0, 0, 0, 255))
-                        d.text((950, 60), n1 + n2, font=font, fill=(0, 0, 0, 255))
+    def generate_plate(self, l1, l2, l3, l4, n1, n2, show_bounding_box=False, show=False, save=True):
+        """
+        Given 4 letters and 2 numbers generate a license plate.
+        Saves the image as a PNG in the results dir.
+        """
+        # Get the image
+        base = Image.open(self.plate_filepath).convert('RGBA')
+        # Make a blank image for the text, initialized to transparent text color
+        txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
+        # Get the font
+        font = ImageFont.truetype(self.font_filepath, self.font_size)
+        # Get a drawing context
+        draw = ImageDraw.Draw(txt)
+        # Draw chars
+        coordinates_and_chars = [(90, 60, l1),
+                                 (270, 60, l2),
+                                 (500, 60, l3),
+                                 (680, 60, l4),
+                                 (940, 60, n1),
+                                 (1100, 60, n2)]
+        for x, y, c in coordinates_and_chars:
+            draw.text((x, y), c, font=font, fill=(0, 0, 0, 255))
 
-                        out = Image.alpha_composite(base, txt)
+        if show_bounding_box:
+            x_offset = 5
+            y_offset = 10
+            width = 170
+            height = 255
+            for x, y, _ in coordinates_and_chars:
+                draw.rectangle([x + x_offset, y + y_offset, x + width, y + height], outline="black")
 
-                        out.save("{0}/{1}".format(result_dir, l1 + l2 + l3 + l4 + n1 + n2 + ".png"), "PNG")
+        out = Image.alpha_composite(base, txt)
+
+        if show: out.show()
+
+        if save: out.save("{0}/{1}".format(self.result_dir, l1 + l2 + l3 + l4 + n1 + n2 + ".png"), "PNG")
+
+    def generate_random_plates(self, n, show_bounding_box=False, show=False, save=True):
+        """
+        Generates n license plates randomly.
+        """
+        for _ in range(n):
+            l1, l2, l3, l4, n1, n2 = self.get_chars()
+            self.generate_plate(l1, l2, l3, l4, n1, n2, show_bounding_box=show_bounding_box, show=show, save=save)
+
+
+# ----- MAIN SCRIPT ----- #
+PlateGenerator().generate_random_plates(1, show_bounding_box=True, show=True, save=False)
